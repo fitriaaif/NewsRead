cdffefebcce4c218f4f97f7d832cf9577747838d
Fitria Amanda <101631102+fitriaaif@users.noreply.github.com>
2022-11-25 15:29:36 +0700

    mobile app

---
+++ NewsRead/Profile.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewsRead
+{
+    /// <summary>
+    /// Interaction logic for Profile.xaml
+    /// </summary>
+    public partial class Profile : Page
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Saved_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Account_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Edit1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SignOut_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ NewsRead/Saved_News.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewsRead
+{
+    /// <summary>
+    /// Interaction logic for Saved_News.xaml
+    /// </summary>
+    public partial class Saved_News : Page
+    {
+        public Saved_News()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Saved_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Account_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Share1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Saved1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
